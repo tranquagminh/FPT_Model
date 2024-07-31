21325b2b6ce2577108d56daa9302225db43a3520
Trần Quang Minh <118581461+tranquagminh@users.noreply.github.com>
2024-07-31 08:27:45 +0000

    "Update chatbot"

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ from flask import Flask, request, render_template
 from chatbot_model.model import QAModel
 
 app = Flask(__name__)
+chat_history = []
 model = QAModel("Fsoft-AIC/videberta-base")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        question = request.form["question"]
-        context = request.form["context"]
+        context = request.form.get("context", "")
+        question = request.form.get("question", "")
         answer = model.get_answer(question, context)
-        return render_template("index.html", question=question, context=context, answer=answer)
+        print(context)
+        if question:
+            chat_history.append({"role": "user", "text": question})
+        if answer:
+            chat_history.append({"role": "bot", "text": answer})
+        return render_template("index.html", chat_history=chat_history, context=context, question=question)
     return render_template("index.html")
 
 if __name__ == "__main__":
